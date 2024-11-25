5f1a62b2f20dfcfeeceb824a79044248d2cf89a0
fernando.hideki <fernando.watanabe@sistemafiep.org.br>
2024-11-25 18:48:10 -0300

    Teste

--- teste.py
+++ teste.py
@@ -2,9 +2,5 @@
 import streamlit as st
 import pandas as pd
 
-st.write("""
-            #Teste
-         """)
-
-df = pd.read_csv("consolidado_tratado.csv")
-st.line_chart(df)
+df = pd.read_csv("consolidado_tratado.csv", delimiter=";")
+st.dataframe(df)
